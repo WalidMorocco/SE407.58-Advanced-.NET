2c9177eec6237de729480ea906f85c356823d1b2
Walid Amar <walidamarpro@gmail.com>
2022-03-08 18:28:44 -0500

    Final course example

---
+++ BlockBuster/BlockBuster/Models/CustomerPartial.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlockBuster.Models
+{
+    [ModelMetadataType(typeof(CustomerMetaData))]
+    public partial class Customer
+    {
+
+    }
+
+    public class CustomerMetaData
+    {
+        [Display(Name ="Phone Number")]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ BlockBuster/BlockBuster/Models/MoviePartial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlockBuster.Models
+{
+    [ModelMetadataType(typeof(MovieMetaData))]
+    public partial class Movie
+    {
+    }
+
+    public class MovieMetaData
+    {
+        [Display(Name = "Release Year")]
+        public int ReleaseYear { get; set; }
+    }
+}
